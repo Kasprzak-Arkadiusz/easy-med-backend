8cd15d51db332ba93e384c8f610f3e977dd64898
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2022-04-14 08:15:55 +0200

    Change min. email length to 3

--- src/Api/Api/Dtos/Validators/ValidationConstants.cs
+++ src/Api/Api/Dtos/Validators/ValidationConstants.cs
@@ -4,6 +4,6 @@ public static class ValidationConstants
 {
     public const int MinPasswordLength = 8;
     public const int MaxPasswordLength = 64;
-    public const int MinEmailAddressLength = 5;
+    public const int MinEmailAddressLength = 3;
     public const int MaxEmailAddressLength = 50;
 }
