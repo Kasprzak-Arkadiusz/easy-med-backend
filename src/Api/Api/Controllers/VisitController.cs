9206cc739f8174dec1e16d30820c94726e42e9ee
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2022-04-19 17:21:16 +0200

    Add missing endpoint documentation

--- src/Api/Api/Controllers/VisitController.cs
+++ src/Api/Api/Controllers/VisitController.cs
@@ -17,15 +17,21 @@ public class VisitController : BaseController
     /// <response code="400">Validation or logic error</response>
     [HttpPost]
     [ProducesResponseType(typeof(ReserveVisitViewModel), StatusCodes.Status201Created)]
-    [ProducesResponseType(400)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> Reserve([FromBody] ReserveVisitDto dto)
     {
         var viewModel = await Mediator.Send(new ReserveVisitCommand(dto.VisitDateTime, dto.DoctorId, dto.PatientId));
         return CreatedAtRoute(null, viewModel);
     }
 
-    [HttpDelete("{id}")]
+    /// <summary>
+    /// Cancel a visit
+    /// </summary>
+    /// <response code="200">Visit successfully cancelled</response>
+    /// <response code="400">Validation or logic error</response>
+    [HttpDelete("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Cancel(int id)
     {
         await Mediator.Send(new CancelVisitCommand(id));
