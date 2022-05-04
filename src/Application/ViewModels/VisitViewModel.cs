d31d3ff4866ca20866b1e17503438d6a1a19a447
Krzychu <krzysiek1.kowalski@gmail.com>
2022-05-04 19:24:04 +0200

    Add GetVisitsByDoctorIdQuery

---
+++ src/Application/ViewModels/VisitViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EasyMed.Application.Common.Mappings;
+using EasyMed.Domain.Entities;
+
+namespace EasyMed.Application.ViewModels;
+
+public class VisitViewModel : IMapFrom<Visit>
+{
+    public int Id { get; set; }
+    public DateTime DateTime { get; set; }
+    public bool IsCompleted { get; set; }
+    public int DoctorId { get; set; }
+    public int PatientId { get; set; }
+    public PatientViewModel Patient { get; set; }
+}
