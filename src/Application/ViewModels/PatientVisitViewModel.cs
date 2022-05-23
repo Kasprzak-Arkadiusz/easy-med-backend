268910eb32fd8f5c29e918a744d4bd107bfe83c9
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2022-05-23 13:42:09 +0200

    Revert changes and unify responses

--- src/Application/ViewModels/PatientVisitViewModel.cs
+++ src/Application/ViewModels/PatientVisitViewModel.cs
@@ -7,16 +7,18 @@ namespace EasyMed.Application.ViewModels;
 public class PatientVisitViewModel : IMapFrom<Visit>
 {
     public int Id { get; set; }
-    public DateTime Date { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
     public string Location { get; set; }
     public string DoctorName { get; set; }
     public string MedicalSpecialization { get; set; }
     public bool Completed { get; set; }
-    
+
     public void Mapping(Profile profile)
     {
         profile.CreateMap<Visit, PatientVisitViewModel>()
-            .ForMember(vm => vm.Date, opt => opt.MapFrom(v => v.DateTime))
+            .ForMember(vm => vm.StartDate, opt => opt.MapFrom(v => v.DateTime))
+            .ForMember(vm => vm.EndDate, opt => opt.MapFrom(v => v.DateTime.AddMinutes(Visit.GetVisitTimeInMinutes())))
             .ForMember(vm => vm.Location, opt => opt.MapFrom(v => v.Doctor.OfficeLocation!.Address))
             .ForMember(vm => vm.DoctorName, opt => opt.MapFrom(v => v.Doctor.GetFullName()))
             .ForMember(vm => vm.MedicalSpecialization, opt => opt.MapFrom(v => v.Doctor.MedicalSpecialization))
